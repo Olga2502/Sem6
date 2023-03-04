751c6d817435da863b701e73bc4231239807e1a9
Olga <101519229+Olga2502@users.noreply.github.com>
2023-03-04 16:44:05 +0300

    Решение задачи 44 с числом Фабиначи

---
+++ EX44/Program.cs
@@ -0,0 +1,20 @@
+﻿//Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+Console.Clear();
+
+int[] Fib(int na)
+{
+    int[] arr = new int[na];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i < na; i++)
+    {
+        arr[i] = arr[i - 1]+ arr[i - 2];
+    }
+    return arr;
+}
+
+
+int na = int.Parse(Console.ReadLine()!);
+int[] array = Fib(na);
+Console.WriteLine(String.Join(", ", array));
